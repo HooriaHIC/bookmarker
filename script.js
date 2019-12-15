7d56db18b7e8c2b8b8608d91e59bbc859e64c023
HooriaHIC <hooria.hic@gmail.com>
2019-12-15 16:06:46 +0500

    fixed some issues

--- script.js
+++ script.js
@@ -1,53 +1,65 @@
-document.getElementById('Form').addEventListener('submit', saveBookmark);
+document.getElementById("Form").addEventListener("submit", saveBookmark);
+
 
 function saveBookmark(e) {
+
     let siteName = document.getElementById('siteName').value;
     let siteUrl = document.getElementById('siteUrl').value;
 
-    siteUrl = fixUrl(siteUrl);
     if (!validateForm(siteName, siteUrl)) {
-        e.preventDefault();
         return false;
+
+
     }
+
 
     let bookmark = {
         name: siteName,
         url: siteUrl
     }
+
+
     if (localStorage.getItem('bookmarks') === null) {
         let bookmarks = [];
         bookmarks.push(bookmark);
-        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+        localStorage.setItem("bookmarks", JSON.stringify(bookmarks))
     } else {
         let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
         bookmarks.push(bookmark);
-        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+        localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+
     }
-    fetchBookmarks();
+
     document.getElementById('Form').reset();
     document.getElementById("myModal").style.display = "none";
+    fetchBookmarks();
     e.preventDefault();
 }
-
 function deleteBookmark(url) {
+
     let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-    for (i = 0; i < bookmarks.length; i++) {
+    for (let i = 0; i < bookmarks.length; i++) {
+
         if (bookmarks[i].url == url) {
             bookmarks.splice(i, 1);
+
         }
+
     }
-    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
     fetchBookmarks();
 }
+
 
 function fetchBookmarks() {
     let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
     let bookmarksResults = document.getElementById('bookmarksResults');
     bookmarksResults.innerHTML = '';
-    for (let i = 0; i < bookmarks.length; i++) {
-        let name = bookmarks[i].name;
-        let url = bookmarks[i].url;
 
+    for (let i = 0; i < bookmarks.length; i++) {
+        const name = bookmarks[i].name;
+        const url = bookmarks[i].url;
         bookmarksResults.innerHTML +=
             '<div class="bookmas">' +
             '<h3 id="siteName" class="siteName">' + name + '</h3>' +
@@ -60,27 +72,22 @@ function fetchBookmarks() {
 }
 
 function validateForm(siteName, siteUrl) {
-    let expression = /^((http|https)(:\/\/))?(www.)?(\w+)(.\w{2,6})(.\w{2,6})?$/i
-    let regex = new RegExp(expression);
 
     if (!siteName || !siteUrl) {
-        print("add name and url")
         return false;
-    } else if (!siteUrl.match(regex)) {
+    }
+
+    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+
+    if (!siteUrl.match(regex)) {
         return false;
-    } else {
-        return true;
-    }
-}
 
-function fixUrl(url) {
-    expression = /^(http:\/\/|https:\/\/)/i
-    let regex = new RegExp(expression);
-    if (!regex.test(url)) {
-        return "https://" + url;
     }
-}
 
+    return true;
+
+}
 
 // for the modal
 
